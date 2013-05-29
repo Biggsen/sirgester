29fddbeaad37c4b9320e3b6fa1e4c967a78bd5f3
Stone Gislason <stone@sondevia.is>
2013-05-29 21:48:05 +0100

    split up main file

---
+++ js/model.js
@@ -0,0 +1,94 @@
+var Genre = Parse.Object.extend("Genre", {
+	defaults: {
+		name: '',
+	}
+});
+
+var Genres = Parse.Collection.extend({
+	model: Genre,
+});
+
+
+var Book = Parse.Object.extend("Book", {
+
+	initialize: function() {
+		_.bindAll(this, 'toJSON' );	
+	},
+
+	defaults : {
+		"id": null,
+		"name": "",
+		"genre" : "",
+		"author": "",
+		"totalpages": 100,
+		"currentPage": 0,
+		"shelfed": false
+	},
+
+	
+	//calculated fields
+  	percentage: function() {
+  		return ((parseFloat(this.get("currentPage")) / parseFloat(this.get("totalpages"))) * 100).toFixed(2);
+  	},
+
+	nextMilestone: function () {
+		var milestones = [25,50,75,100];
+		for (var i = 0, len = milestones.length; i < len; i++) {
+			var result = this.pagesToMilestone( milestones[i] );
+			if( result > 0 )
+				return milestones[i];
+		}
+		return 0;
+	},
+
+	pagesToMilestone: function ( milestone ) {
+	    var result  = this.mileStonePage( milestone ) - this.get("currentPage");
+		if( result < 0 )
+			return 0;
+		return result;
+	},
+
+	mileStonePage: function ( milestone ) {
+		return (( milestone * this.get("totalpages") ) / 100).toFixed(0);
+	},
+
+	pagesToNextMilestone: function ( ) {
+	    var result  = this.mileStonePage( this.nextMilestone() ) - this.get("currentPage");
+		if( result < 0 )
+			return 0;
+		return result;
+	},
+
+	percentageLeft: function() {
+		var milestone = this.nextMilestone();
+		var perc = this.percentage();
+		return (milestone - perc).toFixed(2);
+	},
+
+  	//http://kilon.org/blog/2012/02/backbone-calculated-fields/
+  	toJSON: function() {
+    	var data = {};
+    	var json = Parse.Object.prototype.toJSON.call(this);
+    	_.each(json, function(value, key) {
+      		data[key] = this.get(key);
+    	}, this);
+    	data["objectId"] =  this.id;
+    	data["createdAt"] =  this.createdAt;
+    	data["updatedAt"] =  this.updatedAt; 
+    	var nextMileStone = this.nextMilestone();
+    	data["percentage"] =  parseFloat(this.percentage()).toFixed(0); 
+    	data["nextMilestone"] = nextMileStone;
+    	data["pagesToNextMilestone"] = this.pagesToNextMilestone();
+    	data["percentageLeft"] = parseFloat(this.percentageLeft()).toFixed(0);
+    	data["pagesToMilestone"] = this.mileStonePage(nextMileStone);
+    	return data;
+  	},
+  	/*get: function(attr) {
+    	var value = Parse.Object.prototype.get.call(this, attr);
+    	return _.isFunction(value) ? value.call(this) : value;
+  	},*/
+});
+
+var Books = Parse.Collection.extend({
+	model: Book
+});
---
+++ js/utils.js
@@ -0,0 +1,97 @@
+//TODO: more generic handling of error messages
+function clearMessage() {
+    $("#message").empty();
+}
+
+function displayMessage(message) {
+    clearNotification();
+    clearMessage();
+    $("#message").append(message);
+}
+
+var notice;
+
+function clearNotification() {
+
+    new EmptyView();
+
+}
+
+/*<!-- Error -->
+<div class="notice error"><i class="icon-remove-sign icon-large"></i> This is an Error Notice 
+<a href="#close" class="icon-remove"></a></div>
+
+<!-- Warning -->
+<div class="notice warning"><i class="icon-warning-sign icon-large"></i> This is a Warning Notice 
+<a href="#close" class="icon-remove"></a></div>
+
+<!-- Success -->
+<div class="notice success"><i class="icon-ok icon-large"></i> This is a Success Notice 
+<a href="#close" class="icon-remove"></a></div>*/
+function displaySuccess(message) {
+	
+	new NotificationView({
+					type: 'success',
+					icon: 'ok',
+					text: message
+				});
+}
+
+function displayWarning(message) {
+	new NotificationView({
+					type: 'warning',
+					icon: 'warning',
+					text: message
+				});	
+}
+
+function displayError(message) {
+	
+	new NotificationView({
+					type: 'error',
+					icon: 'remove',
+					text: message
+				});
+}
+
+String.prototype.endsWith = function(pattern) {
+    var d = this.length - pattern.length;
+    return d >= 0 && this.lastIndexOf(pattern) === d;
+};
+
+tpl = {
+ 
+    // Hash of preloaded templates for the app
+    templates:{},
+ 
+    // Recursively pre-load all the templates for the app.
+    // This implementation should be changed in a production environment. All the template files should be
+    // concatenated in a single file.
+    loadTemplates:function (names, callback) {
+ 
+        var that = this;
+ 
+        var loadTemplate = function (index) {
+            var name = names[index];
+            console.log('Loading template: ' + name);
+
+            $.get('templates/' + name + '.html', function (data) {
+                that.templates[name] = data;
+                index++;
+                if (index < names.length) {
+                    loadTemplate(index);
+                } else {
+                    callback();
+                }
+            });
+        }
+ 
+        loadTemplate(0);
+    },
+ 
+    // Get template by name from hash of preloaded templates
+    get:function (name) {
+        return this.templates[name];
+    }
+ 
+};
