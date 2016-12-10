68050cbd6926fe7cdc42db051e75a36026aa7686
Stone Gislason <stone.gislason@drfoster.com>
2016-12-10 14:15:27 +0000

    milestone calculations

--- components/books-page/books.js
+++ components/books-page/books.js
@@ -27,6 +27,8 @@ define(['knockout', 'text!./books.html'], function(ko, template){
         },
       })
       .done(function(data) {
+        //console.log(data);
+        self.update(data);
         self.books(data);
       })
       .fail(function(er) {
@@ -35,6 +37,55 @@ define(['knockout', 'text!./books.html'], function(ko, template){
       .always(function() {
         //console.log( "complete" );
       });
+
+      this.update = function (books) {
+        $.each(books, function (index, record) {
+          record.left = record.total - record.current;
+
+          record.mileStonePage = self.nextMilestone(record.total, record.current);
+          record.nextMileStone = self.pagesToNextMilestone(record.total, record.current);
+          record.percentage = self.percentage(record.total, record.current);
+          record.percentageLeft = self.percentageLeft(record.total, record.current);
+        });
+      };
+
+      this.percentage = function(total, current) {
+        return ((current / total) * 100).toFixed(2);
+      };
+
+      this.percentageLeft = function(total, current) {
+        var milestone = self.nextMilestone(total, current);
+        var perc = self.percentage(total, current);
+        return (milestone - perc).toFixed(2);
+      };
+
+      this.pagesToNextMilestone = function ( total, current ) {
+        var result  = self.mileStonePage( self.nextMilestone( total, current), total ) - current;
+        if( result < 0 )
+          return 0;
+        return result;
+      };
+
+      this.nextMilestone = function (total, current) {
+        var milestones = [25,50,75,100];
+        for (var i = 0, len = milestones.length; i < len; i++) {
+          var result = self.pagesToMilestone( milestones[i], total, current );
+          if( result > 0 )
+            return milestones[i];
+        }
+        return 0;
+      };
+
+      this.pagesToMilestone = function ( milestone, total, current ) {
+          var result  = self.mileStonePage( milestone, total ) - current;
+        if( result < 0 )
+          return 0;
+        return result;
+      };
+
+      this.mileStonePage = function ( milestone, total ) {
+        return (( milestone * total ) / 100).toFixed(0);
+      };
   }
 
   return { viewModel: BooksViewModel, template: template }
