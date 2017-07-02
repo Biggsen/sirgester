4fdc2aae7ed2f6eb2e4b421e3e34186f0e2207c1
Biggs Thorarensen <biggs@devillion.com>
2017-07-02 15:37:58 +0100

    icon fonts, menu, gulp, npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,12 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var harp        = require('harp');
+var gulp = require('gulp');
+var gulpCopy = require('gulp-copy');
 
-/**
- * Serve the Harp Site from the src directory
- */
-gulp.task('serve', function () {
-  harp.server(__dirname, {
-    port: 9000
-  }, function () {
-    browserSync({
-      proxy: "localhost:9000",
-      open: false,
-      /* Hide the notification. It gets annoying */
-      notify: {
-        styles: ['opacity: 0', 'position: absolute']
-      }
-    });
-    /**
-     * Watch for scss changes, tell BrowserSync to refresh main.css
-     */
-    gulp.watch("scss/**/*.scss", function () {
-      reload("main.css", {stream: true});
-    });
-    /**
-     * Watch for all other changes, reload the whole page
-     */
-    gulp.watch(["components/**/*.jade"], function () {
-      reload();
-    });
-  })
+// fonts
+gulp.task('fonts', function () {
+  return gulp.src('bower_components/outline/fonts/*')
+    .pipe(gulp.dest('fonts'));
 });
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the harp site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['serve']);
+gulp.task('default', function(){
+
+});
