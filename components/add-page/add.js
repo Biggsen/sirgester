c9e7901109ed2d04df7c96d4d3c1814d3d3748f0
Stone Gislason <stone.gislason@drfoster.co.uk>
2017-07-02 13:44:35 +0000

    Add genres to new book

--- components/add-page/add.js
+++ components/add-page/add.js
@@ -1,12 +1,23 @@
-define(['knockout', 'text!./add.html', 'api', 'utils'], function(ko, template, api, utils){
+define(['knockout', 'text!./add.html', 'jquery', 'api', 'utils'], function(ko, template, $, api, utils){
 
   function AddViewModel(route) {
-    this.title = ko.observable("Forgive me, I just sold my only friend!);
+    var self = this;
+
+    this.title = ko.observable("Forgive me, I just sold my only friend!");
     this.firstName = ko.observable("Stone");
     this.lastName = ko.observable("Gislason");
-    this.genres = ko.observableArray(["Sci-Fi", "Action", "Dev"]);
+    this.genres = ko.observableArray([]);
     this.total = ko.observable(150);
     this.current = ko.observable(3);
+
+    var url = '/genre';
+    api.get(url, function(data){
+      $.map(data, function( val, i ) {
+        self.genres.push(val);
+      });
+
+      self.genres.sort(utils.sort('name', 'asc'));
+    });
 
     this.savebook = function() {
       alert('saving book');
